e19c3601816b117c427fbc01d0101ec9915c0939
VictorCassara <cassaravictor@hotmail.com>
2015-08-26 07:59:30 -0300

    renamed the packages so now it is easy to identify what you're using. The new controller class can update the XML configuration file. Fix'd the configuration file not being loaded.

---
+++ src/mylogger/controller/Logger.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package mylogger.controller;
+
+import mylogger.dao.LogDAO;
+import mylogger.dao.impl.LogDAOImpl;
+import mylogger.util.exception.ConfigurationException;
+
+/**
+ *
+ * @author <a href="mailto:victor.s.cassara@ericssoninovacao.com.br">Victor Cassara</a>
+ * @version $Id: filename,v x.x.x.x YYYY/MM/DD hh:mm:ss uname Exp $
+ *
+ */
+public class Logger {
+  private LogDAO log;
+  
+  public Logger(){
+    log = new LogDAOImpl();
+    try {
+      log.loadConfiguration();
+    } catch (ConfigurationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Our log functionalities object.
+   */
+  
+  /**
+   * Method responsible for writing the message.
+   * @param message
+   */
+  public void message(String message){
+    log.message(message);
+  }
+  
+  /**
+   * Method responsible for writing the message with error.
+   * @param message
+   */
+  public void error(String message){
+    log.errorMessage(message);
+  }
+  
+  /**
+   * Method responsible for writing the message with warning.
+   * @param message
+   */
+  public void warning(String message){
+    log.warningMessage(message);
+  }
+  
+  /**
+   * Method responsible for writing the new project name, auto clean option and file directory
+   * @param projectName
+   * @param autoClean
+   * @param fileDirectory
+   */
+  public void newConfig(String projectName, String autoClean, String fileDirectory){
+    try {
+      log.saveConfiguration(projectName, autoClean, fileDirectory);
+    } catch (ConfigurationException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/mylogger/util/Constants.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package mylogger.util;
+
+/**
+ *
+ * @author <a href="mailto:victor.s.cassara@ericssoninovacao.com.br">Victor Cassara</a>
+ * @version $Id: filename,v x.x.x.x YYYY/MM/DD hh:mm:ss uname Exp $
+ *
+ */
+public class Constants {
+  
+  /**
+   * Variable that defines the name of the log file that is stored under the user's directory.
+   */
+  private final static String FILE_NAME = "log.txt";
+  
+  /**
+   * Variable that defines the tag of an error message in the log.
+   */
+  private static final String ERROR_TAG = "[ERROR]";
+  
+  /**
+   * Variable that defines the tag of an warning message in the log.
+   */
+  private static final String WARNING_TAG = "[WARNING]";
+  
+  /**
+   * Variable that defines the tag of an ok message in the log.
+   */
+  private static final String OK_TAG = "[OK]";
+  
+  
+  /**
+   * Getter responsible for retrieving the ok tag that shows up inside the log file.
+   * @return String containing the ok tag value (which is a constant defined by the developer)
+   */
+  public static String getOkTag() {
+    return OK_TAG;
+  }
+
+  /**
+   * Getter responsible for retrieving the name of the file that is stored under the user's directory/PlatformSense.
+   * @return String containing the file name (which is a constant defined by the developer)
+   */
+  public static String getFILENAME() {
+    return FILE_NAME;
+  }
+
+  /**
+   * Getter responsible for retrieving the error tag that shows up inside the log file.
+   * @return String containing the error tag value (which is a constant defined by the developer)
+   */
+  public static String getError() {
+    return ERROR_TAG;
+  }
+
+  /**
+   * Getter responsible for retrieving the warning that shows up inside the log file.
+   * @return String containing the warning tag value (which is a constant defined by the developer)
+   */
+  public static String getWarning() {
+    return WARNING_TAG;
+  }
+  
+  
+  
+}
